8d167eb8858b5c6007c46221966afd4da5ca310a
Hannah Klein <hannahklein@posteo.de>
2020-06-14 16:14:04 +0200

    while loop removed

--- secretnumber.py
+++ secretnumber.py
@@ -5,11 +5,3 @@ if secret == guess:
     print("Congrats. Correct guess.")
 else:
     print("Sorry. Not the secret number.")
-
-
-while secret != guess:
-    print("Sorry. Not the secret number.")
-    guess = int(input("Please enter a new number: "))
-    if secret == guess:
-        print("Yes")
-        break
